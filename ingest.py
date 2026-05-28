b9e0a10c8f307a15e11c270f95fa14646943b0f9
Kinyanjui <wkabucho42gmail.com>
2026-05-28 22:25:57 +0300

    Added documents

--- ingest.py
+++ ingest.py
@@ -4,7 +4,7 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 PDF_FOLDER = "pdfs"
 
